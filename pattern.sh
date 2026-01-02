f0ba4ce34849ea58629929a2058af41df0cef44f
acrym48 <646946455z@gmail.com>
2026-01-03 04:58:01 +0500

    Add pattern.sh

---
+++ pattern.sh
@@ -0,0 +1,15 @@
+DISTRO_NAME=""
+DISTRO_COMMENT=""
+
+TARBALL_URL['aarch64']=""
+TARBALL_SHA256['aarch64']=""
+TARBALL_URL['arm']=""
+TARBALL_SHA256['arm']=""
+TARBALL_URL['i686']=""
+TARBALL_SHA256['i686']=""
+TARBALL_URL['x86_64']=""
+TARBALL_SHA256['x86_64']=""
+
+distro_setup() {
+	#example: run_proot_cmd echo ""
+}
